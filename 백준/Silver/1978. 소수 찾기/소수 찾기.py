fe7d0d890532600957280b1f82b45fba4df9d126
rokong.me <fhrgud1264@gmail.com>
2023-01-18 11:00:47 +0900

    [Silver V] Title: 소수 찾기, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -6,7 +6,7 @@ cnt = 0
 for i in n:
     if i == 1:
         continue
-    for j in range(2, i):
+    for j in range(2, int(i**0.5) + 1):
         if i % j == 0:
             cnt += 1
             continue
