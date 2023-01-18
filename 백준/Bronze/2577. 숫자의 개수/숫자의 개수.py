2f6f01c4c7d265b17335159ad446a458f809e006
rokong.me <fhrgud1264@gmail.com>
2023-01-18 11:36:18 +0900

    [Bronze II] Title: 숫자의 개수, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,14 @@
+result = 1
+for _ in range(3):
+    result *= int(input())
+result = str(result)
+print(result.count("0"))
+print(result.count("1"))
+print(result.count("2"))
+print(result.count("3"))
+print(result.count("4"))
+print(result.count("5"))
+print(result.count("6"))
+print(result.count("7"))
+print(result.count("8"))
+print(result.count("9"))
